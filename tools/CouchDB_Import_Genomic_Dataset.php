8d358d0439fa477d31c8b6d8988affc0203165ea
xlecours <xavierlb.mavan@gmail.com>
2016-07-20 11:28:23 -0400

    Genomic_Browser-expected_file_format

---
+++ tools/CouchDB_Import_Genomic_Dataset.php
@@ -0,0 +1,65 @@
+#!/usr/bin/php
+<?php
+/**
+ * This file should be use to insert a data.frame.csv file in CouchDB.
+ * The arguments should be the file_id of that file.
+ *
+ * PHP Version 5
+ *
+ *  @category Genomics
+ *  @package  Main
+ *  @author   Xavier Lecours Boucher <xavier.lecoursboucher@mcgill.ca>
+ *  @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ *  @link     https://www.github.com/aces/Loris-Trunk/
+ */
+
+require_once __DIR__ . "/../vendor/autoload.php";
+require_once 'generic_includes.php';
+require_once 'CouchDB.class.inc';
+require_once 'Database.class.inc';
+require_once 'Utility.class.inc';
+
+/**
+ * The Dataframe_parser follow the specifications described in the 
+ * modules/genomic_browser/couchdb/examples/data.frame.csv file.
+ *
+ *  @category Genomics
+ *  @package  Main
+ *  @author   Xavier Lecours Boucher <xavier.lecoursboucher@mcgill.ca>
+ *  @license  http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ *  @link     https://www.github.com/aces/Loris-Trunk/
+ */
+
+class Dataframe_Parser {
+
+    function run() 
+    {
+        return true;
+    }
+}
+
+if(!class_exists('UnitTestCase')) {
+
+    // Validate arguments
+    if (count($argv) != 2) {
+        echo "Error : Invalid argument count\n";
+        show_usage();
+    }
+    if (!is_int($argv[1])) {
+        echo "Error : Invalid argument count\n";
+        show_usage();
+    }
+    
+    var_dump($argv);
+    $Runner = new Dataframe_Parser();
+    $Runner->run();
+}
+
+function show_usage() {
+    echo "\nUsage\n";
+    echo "    php CouchDB_Import_Genomic_Dataset.php GenomicFileID\n\n";
+    echo "GenomicFileID\n";
+    echo "    The id of the file in the MySQL genomic_files table.\n\n";
+    die;
+}
+?>

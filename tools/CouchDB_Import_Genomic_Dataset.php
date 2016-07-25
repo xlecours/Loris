b764a7976b19723dcfdfc36705283e3282daf550
xlecours <xavierlb.mavan@gmail.com>
2016-07-25 14:06:32 -0400

    working on CouchDB_Import

--- tools/CouchDB_Import_Genomic_Dataset.php
+++ tools/CouchDB_Import_Genomic_Dataset.php
@@ -30,12 +30,106 @@ require_once 'Utility.class.inc';
  *  @link     https://www.github.com/aces/Loris-Trunk/
  */
 
-class Dataframe_Parser {
+class Dataframe_Parser 
+{
 
-    function run() 
+    var $SQLDB; // reference to the database handler, store here instead
+                // of using Database::singleton in case it's a mock.
+
+    var $CouchDB; // reference to the CouchDB database handler
+
+
+    function __construct()
     {
+        $this->SQLDB   = Database::singleton();
+        $this->CouchDB = CouchDB::singleton();
+    }
+
+    function run($GenomicFileID) 
+    {
+        try {
+
+            $dataframe = new Dataframe($GenomicFileID);
+            $dataframe->loadFile();
+            var_dump($dataframe);
+
+        } catch (Exception $e) {
+            echo 'Caught exception: ',  $e->getMessage(), "\n";
+        }
+        echo "Finish\n";
         return true;
     }
+}
+
+class Dataframe 
+{
+    var $file_id;
+    var $file_name;
+
+    var $meta = array(
+        'doctype'         => 'dataset',
+        'file_format'     => 'data.frame',
+        'variable_type'   => null,
+        'variable_format' => null,
+        'variable_count'  => null,
+        'ad_libitum'      => array()
+    );
+
+    var $headers = [];
+
+    function __construct( $GenomicFileID )
+    {
+        $this->file_id = $GenomicFileID;
+        $mysql_db = Database::singleton();
+        $query = 'SELECT FileName, AnalysisModality from genomic_files WHERE GenomicFileID = :v_file_id';
+        $record = $mysql_db->pselectRow($query, array('v_file_id' => $this->file_id));
+
+        if (0 == count($record)) {
+            throw new Exception('No genomic_files for that GenomicFileID');
+        }
+
+        $this->file_name = $record['FileName'];
+        $this->meta['variable_type'] = $record['AnalysisModality'];
+    }
+
+    function loadFile()
+    {
+       if (!file_exists($this->file_name)) {
+           throw new Exception("File not found\n$this->file_name");
+       } 
+       if (!is_readable($this->file_name)) {
+           throw new Exception("File is not readable\n$this->file_name");
+       } 
+
+       $handle = fopen($this->file_name, "r");
+       $this->_skipComments($handle);
+       $this->_loadInformations($handle);
+       $this->_loadHeaders($handle);
+       fclose($handle);
+
+    }
+
+    private function _skipComments(&$handle)
+    {
+        $pattern = '/^###.*/';
+        $offset = ftell($handle);
+
+        while (preg_match($pattern,fgets($handle))) {
+            $offset = ftell($handle);
+        } 
+
+        fseek($handle, $offset, SEEK_SET);
+    } 
+
+    private function _loadInformations(&$handle)
+    {
+        $bob = fgets($handle);
+    } 
+
+    private function _loadHeaders(&$handle)
+    {
+        $bob = fgets($handle);       
+    } 
 }
 
 if(!class_exists('UnitTestCase')) {
@@ -45,14 +139,14 @@ if(!class_exists('UnitTestCase')) {
         echo "Error : Invalid argument count\n";
         show_usage();
     }
-    if (!is_int($argv[1])) {
-        echo "Error : Invalid argument count\n";
+    if (!is_numeric($argv[1])) {
+        echo "Error : GenomicFileID should be a numeric value\n";
+        echo $argv[1] . "\n";
         show_usage();
     }
     
-    var_dump($argv);
     $Runner = new Dataframe_Parser();
-    $Runner->run();
+    $Runner->run($argv[1]);
 }
 
 function show_usage() {

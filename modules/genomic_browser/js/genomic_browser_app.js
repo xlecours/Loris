c2748707d0a738e1d3738a38f15746d9e9301093
Xavier <xavierlb.mavan@gmail.com>
2016-08-12 16:32:43 -0400

    SatticDatatable in profile tab

--- modules/genomic_browser/js/genomic_browser_app.js
+++ modules/genomic_browser/js/genomic_browser_app.js
@@ -1,12 +1,45 @@
 GenomicBrowserApp = React.createClass({
-    displayName: "GenomicBrowserApp",
+    displayName: 'GenomicBrowserApp',
 
-    render: function () {
+    getInitialState: function () {
+        return {
+            'activetab': 'profile',
+            'headers': [],
+            'data': {}
+        };
+    },
+    componentDidMount: function () {
+        this.loadCurrentTabContent();
+    },
+    loadCurrentTabContent: function () {
+        var that = this;
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (xhttp.readyState == 4 && xhttp.status == 200) {
+                that.setState({ data: JSON.parse(xhttp.responseText) });
+            }
+        };
+        var url = loris.BaseURL.concat('/genomic_browser/ajax/get_', this.state.activetab, '_tab_content.php');
+        xhttp.open("GET", url, true);
+        xhttp.send();
+    },
+    formatCell: function (a, b, c, d) {
         return React.createElement(
-            "h1",
+            'td',
             null,
-            "BOB!"
+            b
         );
+    },
+    render: function () {
+        var headers = ['PSCID', 'Site', 'Visit Label', 'Subproject', 'Sex', 'Date of Birth'];
+        var data = [['0', '0', '0', '0', '0', '0'], ['0', '0', '0', '0', '0', '0']];
+        return React.createElement(StaticDataTable
+        //Headers={this.state.headers}
+        , { Headers: headers
+            //Data={this.state.data}
+            , Data: data,
+            getFormattedCell: this.formatCell
+        });
     }
 });
 RGenomicBrowserApp = React.createFactory(GenomicBrowserApp);

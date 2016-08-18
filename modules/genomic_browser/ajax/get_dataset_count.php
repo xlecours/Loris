8630be5c222e708611cf76881b49196ccef43cff
xlecours <xavierlb.mavan@gmail.com>
2016-08-18 16:10:27 -0400

    working on counts

---
+++ modules/genomic_browser/ajax/get_dataset_count.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * This provide the GenomicBrowser content data of the profile tab
+ * Filters will be applyied to the list/view of CouchDB
+ *
+ * PHP Version 5
+ *
+ *  @category   Loris
+ *  @package    Genomic_Module
+ *  @author     Loris Team <loris.mni@bic.mni.mcgill.ca>
+ *  @contriutor Xavier Lecours boucher <xavier.lecoursboucher@mcgill.ca>
+ *  @license    http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ *  @link       https://github.com/aces/Loris
+ */
+
+$userSingleton =& User::singleton();
+if (!$userSingleton->hasPermission('genomic_browser_view_site')
+    && !$userSingleton->hasPermission('genomic_browser_view_allsites')
+) {
+    header("HTTP/1.1 403 Forbidden");
+    exit;
+}
+
+if(empty($_REQUEST['CandID'])) {
+    header("HTTP/1.1 400 Bad Request");
+    exit;
+}
+
+$params = json_decode($_REQUEST['CandID']);
+if (!is_array($params)) {
+    $candidate_list = array($params);
+} else {
+    $candidate_list = $params;
+}
+
+$candidate_list = array_map(function ($CandID) {
+    return  str_pad(strval($CandID), 6, "0", STR_PAD_LEFT);
+}, $candidate_list); 
+
+$sample_candidate_mapping = addSampleLabels($candidate_list);
+
+$couch = CouchDB::singleton();
+$couch->setDatabase('test_epi');
+$params = array(
+    'reduce' => 'true',
+    'group_level' => 1,
+    'samples' => $sample_labels,
+);
+$result = $couch->queryList('genomic_browser', '', 'sample_label_by_dataset', $params, false);
+
+header("content-type:application/json");
+ini_set('default_charset', 'utf-8');
+echo json_encode($result);
+exit;
+
+function addSampleLabels(&$candidate_list)
+{
+    $DB = Database::singleton();
+    $result =  $DB->pselect(
+        'SELECT 
+            CandID, 
+            GROUP_CONCAT(sample_label) 
+         FROM 
+            genomic_sample_candidate_rel 
+         WHERE 
+            FIND_IN_SET(CandID, :v_candidates) 
+         GROUP BY 
+            CandID', 
+         array('v_candidates' => join(',',$candidate_list))
+    );
+    array_walk($candidate_list, function($CandID, $index, $mysql_result) {
+        $sample_labels = array_filter($mysql_result, function($row) {
+            var_dump($CandID);
+            return $row["CandID"] == $CandID;
+        });
+        var_dump($CandID);
+        var_dump($sample_labels);
+    }, $result);
+}

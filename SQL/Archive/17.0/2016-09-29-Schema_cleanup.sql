aede38769deb22a3010535ff9ead585308365fd7
xlecours <xavierlb.mavan@gmail.com>
2016-09-29 12:46:46 -0400

    todo list in patch

--- SQL/Archive/17.0/2016-09-29-Schema_cleanup.sql
+++ SQL/Archive/17.0/2016-09-29-Schema_cleanup.sql
@@ -1,2 +1,9 @@
 -- TODO :: data_release_permissions -> engine and charset
-
+participant_accounts
+participant_emails
+participant_status_history
+consent_info_history
+ExternalLinkTypes
+ExternalLinks
+data_release
+empty_queries

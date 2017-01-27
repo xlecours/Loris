5b65c2ddabca4dc84435cdef310e23b343f3ec36
Rida CCNA <ridz_1208@hotmail.com>
2017-01-27 13:00:20 -0500

    hierarchy

--- SQL/2016-12-22-notifications.sql
+++ SQL/2016-12-22-notifications.sql
@@ -5,28 +5,45 @@ ALTER TABLE users ADD COLUMN `Phone` varchar(15) default NULL;
 -- Associates modules with the service available for each
 DROP TABLE IF EXISTS `notification_modules`;
 CREATE TABLE `notification_modules` (
-      `ID` int(10) unsigned auto_increment NOT NULL,
+      `id` int(10) unsigned auto_increment NOT NULL,
       `module_name` varchar(100) NOT NULL,
       `operation_type` varchar(100) NOT NULL,
       `description` varchar(255) DEFAULT NULL,
-      `email_service` enum('Y','N') default 'N' NOT NULL,
-      `sms_service` enum('Y','N') default 'N' NOT NULL,
-      `phone_service` enum('Y','N') default 'N' NOT NULL,
-      PRIMARY KEY (`ID`),
+      PRIMARY KEY (`id`),
       KEY (`module_name`),
       UNIQUE(module_name,operation_type)
+) ENGINE=InnoDB DEFAULT CHARSET='utf8';
+
+-- Associates modules with the service available for each
+DROP TABLE IF EXISTS `notification_services`;
+CREATE TABLE `notification_services` (
+      `id` int(10) unsigned auto_increment NOT NULL,
+      `service` VARCHAR(50) NOT NULL,
+      PRIMARY KEY (`id`),
+      UNIQUE(service)
+) ENGINE=InnoDB DEFAULT CHARSET='utf8';
+
+-- saves users preferences for notification type
+DROP TABLE IF EXISTS `notification_modules_services_rel`;
+CREATE TABLE `notification_modules_services_rel` (
+      `module_id` int(10) unsigned NOT NULL,
+      `service_id` int(10) unsigned NOT NULL,
+      PRIMARY KEY (`module_id`,`service_id`),
+      CONSTRAINT `FK_notification_modules_services_rel_1` FOREIGN KEY (`module_id`) REFERENCES `notification_modules` (`id`),
+      CONSTRAINT `FK_notification_modules_services_rel_2` FOREIGN KEY (`service_id`) REFERENCES `notification_services` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET='utf8';
 
 -- saves users preferences for notification type
 DROP TABLE IF EXISTS `users_notifications_rel`;
 CREATE TABLE `users_notifications_rel` (
-      -- `ID` int(10) unsigned NOT NULL auto_increment,
       `user_id` int(10) unsigned NOT NULL,
-      `notification_id` int(10) unsigned NOT NULL,
-      `email` enum('Y','N') default 'N' NOT NULL,
-      `sms` enum('Y','N') default 'N' NOT NULL,
-      `phone` enum('Y','N') default 'N' NOT NULL,
-      PRIMARY KEY (`user_id`,`notification_id`),
+      `module_id` int(10) unsigned NOT NULL,
+      `service_id` int(10) unsigned NOT NULL,
+      PRIMARY KEY (`user_id`,`module_id`,`service_id`),
       CONSTRAINT `FK_notifications_users_rel_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`ID`),
-      CONSTRAINT `FK_notifications_users_rel_2` FOREIGN KEY (`notification_id`) REFERENCES `notification_modules` (`ID`)
+      CONSTRAINT `FK_notifications_users_rel_2` FOREIGN KEY (`module_id`) REFERENCES `notification_modules` (`id`),
+      CONSTRAINT `FK_notifications_users_rel_3` FOREIGN KEY (`service_id`) REFERENCES `notification_services` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET='utf8';
+
+
+

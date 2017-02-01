7afa9426ab7146c8f2079f1d9018171fd438eebc
ridz1208 <ridz_1208@hotmail.com>
2017-02-01 16:08:53 -0500

    Schema changes

--- SQL/0000-00-00-schema.sql
+++ SQL/0000-00-00-schema.sql
@@ -1318,6 +1318,11 @@ CREATE TABLE `users` (
   `Country` varchar(255) default NULL,
   `Fax` varchar(255) default NULL,
   `Email` varchar(255) NOT NULL default '',
+<<<<<<< e6f7396fe574a1687c641060b21f6fd8c35bf07f
+=======
+  `Phone` varchar(15) default NULL,
+  `CenterID` tinyint(2) unsigned default NULL,
+>>>>>>> Schema changes
   `Privilege` tinyint(1) NOT NULL default '0',
   `PSCPI` enum('Y','N') NOT NULL default 'N',
   `DBAccess` varchar(10) NOT NULL default '',
@@ -2362,6 +2367,66 @@ CREATE TABLE `user_psc_rel` (
   CONSTRAINT `FK_user_psc_rel_2` FOREIGN KEY (`CenterID`) REFERENCES `psc` (`CenterID`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `FK_user_psc_rel_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 INSERT INTO user_psc_rel (UserID, CenterID) SELECT 1, CenterID FROM psc;
+
+-- Associates modules with the service available for each
+DROP TABLE IF EXISTS `notification_modules`;
+CREATE TABLE `notification_modules` (
+      `id` int(10) unsigned auto_increment NOT NULL,
+      `module_name` varchar(100) NOT NULL,
+      `operation_type` varchar(100) NOT NULL,
+      `description` varchar(255) DEFAULT NULL,
+      PRIMARY KEY (`id`),
+      KEY (`module_name`),
+      UNIQUE(module_name,operation_type)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Associates modules with the service available for each
+DROP TABLE IF EXISTS `notification_services`;
+CREATE TABLE `notification_services` (
+      `id` int(10) unsigned auto_increment NOT NULL,
+      `service` VARCHAR(50) NOT NULL,
+      PRIMARY KEY (`id`),
+      UNIQUE(service)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- saves users preferences for notification type
+DROP TABLE IF EXISTS `notification_modules_services_rel`;
+CREATE TABLE `notification_modules_services_rel` (
+      `module_id` int(10) unsigned NOT NULL,
+      `service_id` int(10) unsigned NOT NULL,
+      PRIMARY KEY (`module_id`,`service_id`),
+      CONSTRAINT `FK_notification_modules_services_rel_1` FOREIGN KEY (`module_id`) REFERENCES `notification_modules` (`id`),
+      CONSTRAINT `FK_notification_modules_services_rel_2` FOREIGN KEY (`service_id`) REFERENCES `notification_services` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- saves users preferences for notification type
+DROP TABLE IF EXISTS `users_notifications_rel`;
+CREATE TABLE `users_notifications_rel` (
+      `user_id` int(10) unsigned NOT NULL,
+      `module_id` int(10) unsigned NOT NULL,
+      `service_id` int(10) unsigned NOT NULL,
+      PRIMARY KEY (`user_id`,`module_id`,`service_id`),
+      CONSTRAINT `FK_notifications_users_rel_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`ID`),
+      CONSTRAINT `FK_notifications_users_rel_2` FOREIGN KEY (`module_id`) REFERENCES `notification_modules` (`id`),
+      CONSTRAINT `FK_notifications_users_rel_3` FOREIGN KEY (`service_id`) REFERENCES `notification_services` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- basic notification service
+INSERT INTO notification_services (service) VALUES
+('email_text');
+
+-- Pre-implemented notifications
+INSERT INTO notification_modules (module_name, operation_type, description) VALUES
+('media', 'upload', 'Media: New File Uploaded'),
+('media', 'download', 'Media: File Downloaded'),
+('document_repository', 'new_category', 'Document Repository: New Category'),
+('document_repository', 'upload', 'Document Repository: New Document Uploaded'),
+('document_repository', 'delete', 'Document Repository: Document Deleted'),
+('document_repository', 'edit', 'Document Repository: Document Edited');
+
+-- Transfer Document repository notifications to new system
+INSERT INTO users_notifications_rel SELECT u.ID, nm.id, ns.id FROM users u JOIN notification_modules nm JOIN notification_services ns WHERE nm.module_name='document_repository' AND ns.service='email_text' AND u.Doc_Repo_Notifications='Y';
 
 

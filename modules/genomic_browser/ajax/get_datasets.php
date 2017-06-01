621d1ddfdb393591ef06dbe1b6e93efffb8543f9
xlecours <xavierlb.mavan@gmail.com>
2017-06-01 14:16:49 -0400

    adding couchdb design document

---
+++ modules/genomic_browser/ajax/get_datasets.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This provide the GenomicBrowser content data of the dataset tab
+ *
+ * PHP Version 5
+ *
+ *  @category   Loris
+ *  @package    Genomic_Module
+ *  @author     Loris Team <loris.mni@bic.mni.mcgill.ca>
+ *  @contriutor Xavier Lecours boucher <xavier.lecoursboucher@mcgill.ca>
+ *  @license    http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ *  @link       https://github.com/aces/Loris
+ */
+
+$userSingleton =& User::singleton();
+if (!$userSingleton->hasPermission('genomic_browser_view_site')
+    && !$userSingleton->hasPermission('genomic_browser_view_allsites')
+) {
+    header("HTTP/1.1 403 Forbidden");
+    exit;
+}
+
+$couch = \NDB_Factory::singleton()->couchDB("genomics");
+
+$params = array('reduce' => 'false');
+$result = $couch->queryView('genomic_browser', 'datasets', $params, false);
+
+header("content-type:application/json");
+ini_set('default_charset', 'utf-8');
+echo json_encode($result);
+exit;

f4863d5dd92fb6f08fb171e09e548275ec4aaeb8
xlecours <xavier.lecoursboucher@mcgill.ca>
2017-11-10 10:19:39 -0500

    psr_17
    https://github.com/php-fig/fig-standards/tree/master/proposed/http-factory

---
+++ php/libraries/http/ResponseFactoryInterface.php
@@ -0,0 +1,16 @@
+<?php
+namespace Psr\Http\Message;
+
+use Psr\Http\Message\ResponseInterface;
+
+interface ResponseFactoryInterface
+{
+    /**
+     * Create a new response.
+     *
+     * @param integer $code HTTP status code
+     *
+     * @return ResponseInterface
+     */
+    public function createResponse($code = 200);
+}

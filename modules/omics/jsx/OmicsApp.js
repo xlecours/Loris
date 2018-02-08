5cbdb45b0b8e7ad44203276071e19bd5fc34b5aa
xlecours <xavier.lecoursboucher@mcgill.ca>
2018-02-08 13:54:01 -0500

    first tabs

--- modules/omics/jsx/OmicsApp.js
+++ modules/omics/jsx/OmicsApp.js
@@ -12,13 +12,60 @@ class OmicsApp extends React.Component {
     this.fetchDatasets = this.fetchDatasets.bind(this);
   }
 
+  componentDidMount() {
+    this.fetchDatasets();
+  }
+
   fetchDatasets() {
-    return;
+    const url = loris.BaseURL.concat('/omics/datasets?format=json');
+    const xhr = new XMLHttpRequest();
+    const that = this;
+
+    xhr.onreadystatechange = function (e) {
+      try {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+          if (xhr.status === 200) {
+            const datasets = xhr.response.Data.map(function (d) {
+              let obj = {};
+              xhr.response.Headers.forEach(function(h,i) {
+                obj[h] = d[i]; 
+              }, this )
+              return obj;
+            },this);
+            that.setState({
+              isLoaded: true,
+              datasets: datasets
+            });
+          } else {
+            console.error('There was a problem with the request.');
+          }
+        }
+      } catch( e ) {
+        console.error(e);
+      }
+    }, this;
+    xhr.open('GET', url);
+    xhr.responseType = 'json';
+    xhr.send();
   }
 
   render() {
+    const datasets = this.state.datasets;
+    const tabList = [{id: 'default', label: 'Datasets'}].concat(datasets.map(function(d) {
+      return ({
+        id: d.Id,
+        label: d.Identifier.variable_type
+      });
+    }));
+    const tabs = [<TabPane key='d' TabId='default' />].concat(datasets.map(function(d, i) {
+      return (
+        <TabPane key={i} TabId={d.Id} />
+      );
+    }));
     return (
-      <h1>BOB</h1>
+      <Tabs tabs={tabList}>
+        {tabs}
+      </Tabs>
     );
   }
 }
@@ -26,7 +73,7 @@ class OmicsApp extends React.Component {
 $(function() {
   const app = (
     <div className="omics-app">
-      <OmicsApp DataURL={`${loris.BaseURL}/omics/`} />
+      <OmicsApp />
     </div>
   );
 

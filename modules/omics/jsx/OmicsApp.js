10a77a0640649f8611acdf10fcc73b97604deb71
xlecours <xavier.lecoursboucher@mcgill.ca>
2018-02-14 14:23:35 -0500

    working with tabs

--- modules/omics/jsx/OmicsApp.js
+++ modules/omics/jsx/OmicsApp.js
@@ -5,12 +5,17 @@ class OmicsApp extends React.Component {
   constructor(props) {
     super(props);
 
+    loris.hiddenHeaders = ['Fileset Id', 'Couch Doc'];
     this.state = {
       isLoaded: false,
+      headers: [],
       datasets: []
     }
 
     this.fetchDatasets = this.fetchDatasets.bind(this);
+    this.formatCell    = this.formatCell.bind(this);
+    this.viewDetails   = this.viewDetails.bind(this);
+    this.goToEditPage  = this.goToEditPage.bind(this);
   }
 
   componentDidMount() {
@@ -32,9 +37,10 @@ class OmicsApp extends React.Component {
                 obj[h] = d[i]; 
               }, this )
               return obj;
-            },this);
+            },this)
             that.setState({
               isLoaded: true,
+              headers: xhr.response.Headers.concat(['Actions']),
               datasets: datasets
             });
           } else {
@@ -50,30 +56,79 @@ class OmicsApp extends React.Component {
     xhr.send();
   }
 
+  viewDetails(event) {
+    const filesetId = event.target.getAttribute("data-fileset-id");
+    this.refs.tabs.handleClick(filesetId, event);
+  }
+
+  goToEditPage(event) {
+    console.log(event.target);
+  }
+
+  formatCell(column, cell, rowData, rowHeaders) {
+    // If a column if set as hidden, don't display it
+    if (loris.hiddenHeaders.indexOf(column) > -1) {
+      return null;
+    }
+
+    // Create the mapping between rowHeaders and rowData in a row object.
+    var row = {};
+    rowHeaders.forEach(function(header, index) {
+      row[header] = rowData[index];
+    }, this);
+
+    let element;
+
+    switch (column) {
+      case 'Origin':
+      case 'ShortDescription':
+      case 'Timestamp Added':
+        element = (
+          <td>{cell}</td>
+        );
+        break;
+      case 'Actions':
+        element = (
+          <td>
+            <button onClick={this.viewDetails} data-fileset-id={row['Fileset Id']}>View</button>
+            &nbsp;
+            <button onClick={this.goToEditPage} data-fileset-id={row['Fileset Id']}>Edit</button>
+          </td>
+        );
+        break;
+    }
+
+    return element;
+  }
+
   render() {
     const datasets = this.state.datasets;
-
     const tabList = [{id: 'default', label: 'Datasets'}].concat(datasets.map(function(d) {
+      const label = (d['Couch Doc']) ? d['Couch Doc'].meta.variable_type : "Unknown";
       return ({
-        id: d.Id,
-        label: d.Identifier.variable_type
+        id: d['Fileset Id'],
+        label: label
       });
     }));
 
     const datasetsTabPane = (
-      <TabPane key='d' TabId='default'>
-        <DatasetsTab datasets={datasets} />
+      <TabPane key='datasets' TabId='default'>
+        <StaticDataTable
+          Headers={this.state.headers}
+          Data={datasets.map(function(d) {return Object.values(d);})}
+          getFormattedCell={this.formatCell}
+        />
       </TabPane>
     );
 
     const tabs = [datasetsTabPane].concat(datasets.map(function(d, i) {
       return (
-        <TabPane key={i} TabId={d.Id} />
+        <TabPane key={i} TabId={d['Fileset Id']} />
       );
     }));
 
     return (
-      <Tabs tabs={tabList}>
+      <Tabs tabs={tabList} updateURL={true} ref="tabs">
         {tabs}
       </Tabs>
     );

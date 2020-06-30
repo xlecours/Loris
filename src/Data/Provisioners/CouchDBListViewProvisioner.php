351d52a5bb6aadbc9f4067011b90d3b85e077273
xlecours <xavier.lecoursboucher@mcgill.ca>
2020-06-30 14:51:50 -0400

    now using couchdb list

---
+++ src/Data/Provisioners/CouchDBListViewProvisioner.php
@@ -0,0 +1,178 @@
+<?php declare(strict_types=1);
+/**
+ * PHP Version 7
+ *
+ * @category   Data
+ * @package    Main
+ * @subpackage Data
+ * @author     Xavier Lecours <xavier.lecours@mcin.ca>
+ * @license    http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ * @link       https://www.github.com/aces/Loris/
+ */
+
+namespace LORIS\Data\Provisioners;
+
+use \LORIS\Data\Models\CouchDBViewRow;
+use \LORIS\Data\Filters\DefaultFilter;
+use \LORIS\Data\Mappers\DefaultMapper;
+
+/**
+ * A CouchDBViewProvisioner is an instance of ProvisionerInstance which
+ * queries a couchdb view and returns the result as a Traversable.
+ *
+ * @category   Data
+ * @package    Main
+ * @subpackage Data
+ * @author     Xavier Lecours <xavier.lecours@mcin.ca>
+ * @license    http://www.gnu.org/licenses/gpl-3.0.txt GPLv3
+ * @link       https://www.github.com/aces/Loris/
+ */
+abstract class CouchDBListViewProvisioner extends \LORIS\Data\ProvisionerInstance
+{
+    private $_couchdb;
+    private $_config;
+    private $_designdoc;
+    private $_list;
+    private $_view;
+    private $_params;
+    private $_filter;
+    private $_mapper;
+
+    /**
+     * Constructor
+     *
+     */
+    public function __construct(string $designdoc, string $list, string $view, array $params)
+    {
+        $factory = \NDB_Factory::singleton();
+        $config  = $factory->config()->getSetting('CouchDB');
+        $this->_config = $config;
+
+        $this->_couchdb   = $factory->couchdb(
+            $config['dbName'],
+            $config['hostname'],
+            intval($config['port']),
+            $config['admin'],
+            $config['adminpass']
+        );
+        $this->_designdoc = $designdoc;
+        $this->_list      = $list;
+        $this->_view      = $view;
+        $this->_params    = $params;
+        $this->_filter    = new DefaultFilter();
+        $this->_mapper    = new DefaultMapper();
+    }
+
+    public function withParams(array $params): CouchDBListViewProvisioner
+    {
+        $new = clone($this);
+        $new->_params = $params;
+        return $new;
+    }
+
+    public function withAddedFilter($filter): CouchDBListViewProvisioner
+    {
+        // TODO :: rework this to use the filter interface.
+        $new = clone($this);
+        $new->_filter = $filter;
+        return $new;
+    }
+
+    public function withMapper($mapper): CouchDBListViewProvisioner
+    {
+        // TODO :: rework this to use the mapper interface.
+        $new = clone($this);
+        $new->_mapper = $mapper;
+        return $new;
+    }
+
+    /**
+     * Returns all the items in the rows property of the results.
+     *
+     * @return \Traversable
+     */
+    public function getAllInstances() : \Traversable
+    {
+        $handler = $this->sendQuery();
+        while(!$handler->eof()) {
+            $line = $handler->gets() ?: '';
+
+            // HTTP error handling
+            if (preg_match('/^HTTP\/1.0 [45]/', $line, $matches)) {
+                while(!$handler->eof()) {
+                    error_log($line);
+                    $line = $handler->gets();
+                }
+                error_log(print_r(get_object_vars($this),true));
+                throw new \Error($line);
+            }
+
+            if (preg_match('/^(\{.*}),*/', $line, $matches)) {
+                // This is a data line
+                if ($this->_filter($matches[1])) {
+                    yield $this->_map($matches[1]);
+                }
+            }
+        }
+    }
+
+    private function _filter($value): bool {
+        return $this->_filter->filter($value);
+    }
+
+    private function _map($value): string {
+        return $this->_mapper->map($value);
+    }
+
+    /**
+     * Send the http request and return the SocketWrapper from which
+     * the response can be read.
+     *
+     * When keys are provided in the params, a POST request will
+     * be sent instead of a GET. This allow to send more data because the
+     * keys will be sent in the request body.
+     *
+     * see: https://docs.couchdb.org/en/stable/api/ddoc/views.html
+     *
+     * @return \SocketWrapper
+     */
+    protected function sendQuery(): \SocketWrapper
+    {
+        $method  = 'GET';
+        $payload = '';
+
+        $handler = $this->_couchdb->SocketHandler;
+        $handler->setHost($this->_config['hostname']);
+        $handler->setPort(intval($this->_config['port']));
+        $handler->open();
+
+        if (isset($this->_params['keys'])) {
+            $payload = json_encode(
+                ["keys" => $this->_params['keys']]
+            );
+            $method = 'POST';
+
+            $this->_params['keys'] = null;
+        }
+
+        $url   = "_design/" . $this->_designdoc . "/_list/" . $this->_list . '/' . $this->_view
+                     . "?" . http_build_query($this->_params);
+
+        $handler->write(
+            $this->_couchdb->_constructURL($method, $url) . " HTTP/1.0\r\n"
+        );
+        $handler->write(
+            "Authorization: Basic "
+            . base64_encode(
+                $this->_config['admin'] . ":" . $this->_config['adminpass']
+            )
+            . "\r\n"
+        );
+
+        $handler->write("Content-Length: " . strlen($payload) . "\r\n");
+        $handler->write("Content-type: application/json\r\n");
+        $handler->write("\r\n$payload");
+
+        return $handler;
+    }
+}
